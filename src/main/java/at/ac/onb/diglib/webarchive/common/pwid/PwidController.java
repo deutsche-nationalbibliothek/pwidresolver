2d9fa8aada405013049095596188a995ca3d852f
Natanael Arndt <arndtn@gmail.com>
2025-10-16 18:08:20 +0200

    adjust example curl calls in comment

--- src/main/java/at/ac/onb/diglib/webarchive/common/pwid/PwidController.java
+++ src/main/java/at/ac/onb/diglib/webarchive/common/pwid/PwidController.java
@@ -77,11 +77,11 @@ public class PwidController {
 
 
     /*
-     curl -v "http://localhost:8080/pwid?archiveString=urn:pwid:webarchiv.onb.ac.at:2013-12-03T17:03:03Z:page:http://m.onb.ac.at/prunksaal.htm"
-     curl -v "http://localhost:8080/pwid?archiveString=https://webarchiv.onb.ac.at/web/20131203170303/http://m.onb.ac.at/prunksaal.htm"
+     curl -v "http://localhost:8080/resolve?pwid=urn:pwid:webarchiv.onb.ac.at:2013-12-03T17:03:03Z:page:http://m.onb.ac.at/prunksaal.htm"
+     curl -v "http://localhost:8080/resolve?pwid=https://webarchiv.onb.ac.at/web/20131203170303/http://m.onb.ac.at/prunksaal.htm"
 
-     curl -v "http://localhost:8080/pwid?archiveString=urn:pwid:archive.org:2022-11-27T18:33:21Z:page:https://www.iana.org/assignments/urn-formal/pwid"
-     curl -v "http://localhost:8080/pwid?archiveString=https://web.archive.org/web/20221127183321/https://www.iana.org/assignments/urn-formal/pwid"
+     curl -v "http://localhost:8080/resolve?pwid=urn:pwid:archive.org:2022-11-27T18:33:21Z:page:https://www.iana.org/assignments/urn-formal/pwid"
+     curl -v "http://localhost:8080/resolve?pwid=https://web.archive.org/web/20221127183321/https://www.iana.org/assignments/urn-formal/pwid"
      */
     @GetMapping(path = "/resolve")
     @Operation(summary = "", description = "This method convert a pwid or a ArchiveUrl via a pwid Objectto a replay URL", hidden = false)
