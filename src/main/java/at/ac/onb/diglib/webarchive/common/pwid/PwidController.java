5c142872871599e55cf43302ec4ff029722a1de2
Natanael Arndt <arndtn@gmail.com>
2025-10-20 11:38:51 +0200

    Remove unused imports

--- src/main/java/at/ac/onb/diglib/webarchive/common/pwid/PwidController.java
+++ src/main/java/at/ac/onb/diglib/webarchive/common/pwid/PwidController.java
@@ -1,12 +1,8 @@
 package at.ac.onb.diglib.webarchive.common.pwid;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
