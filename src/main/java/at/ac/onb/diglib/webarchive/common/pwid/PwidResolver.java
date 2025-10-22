1dad3e6ef56d284a2ffc6274e05dccbb4fc15ede
Natanael Arndt <arndtn@gmail.com>
2025-10-22 14:13:45 +0200

    Return replay url based on pattern

--- src/main/java/at/ac/onb/diglib/webarchive/common/pwid/PwidResolver.java
+++ src/main/java/at/ac/onb/diglib/webarchive/common/pwid/PwidResolver.java
@@ -1,7 +1,20 @@
 package at.ac.onb.diglib.webarchive.common.pwid;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.TimeZone;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+import at.ac.onb.diglib.webarchive.common.pwid.data.Resolver;
 
 public class PwidResolver {
 	protected static final Log log = LogFactory.getLog(PwidResolver.class);
@@ -47,16 +60,51 @@ public class PwidResolver {
     	return registry.isArchiveSupported(pwid.getArchiveId());
     }
 
-    private String getCaptureWithUrl(PWID pwid) {
-    	return pwid.getTimestamp14() + "/" + pwid.getUri();
-    }
-
+	/**
+	 * Get the resolved replay URL for a given PWID.
+	 * @param pwid
+	 * @return The replay URL
+	 */
     public String getResolvedUrl(PWID pwid) {
-		String baseUrl = registry.getReplayBaseUrl(pwid.getArchiveId());
-    	if (baseUrl == null) {
+		Resolver replay = registry.getReplay(pwid.getArchiveId());
+    	if (replay == null) {
     		return null;
-    	}
+		}
 
-    	return baseUrl + getCaptureWithUrl(pwid);
+		return UriComponentsBuilder.fromUriString(replay.getBaseUrl()).path(renderResolverUriTemplate(replay.getPathPattern(), pwid)).build().toUriString();
     }
+
+	public String renderResolverUriTemplate(String template, PWID pwid) {
+		HashMap<String, Object> scopes = new HashMap<String, Object>();
+
+		SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+		SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+		SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+		SimpleDateFormat hourFormat = new SimpleDateFormat("HH");
+		SimpleDateFormat minuteFormat = new SimpleDateFormat("mm");
+		SimpleDateFormat secFormat = new SimpleDateFormat("ss");
+		yearFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		monthFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		dayFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		hourFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		minuteFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		secFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+		scopes.put("archive-id", pwid.getArchiveId());
+		scopes.put("utc-year", yearFormat.format(pwid.timestamp));
+		scopes.put("utc-month", monthFormat.format(pwid.timestamp));
+		scopes.put("utc-day", dayFormat.format(pwid.timestamp));
+		scopes.put("utc-hour", hourFormat.format(pwid.timestamp));
+		scopes.put("utc-minute", minuteFormat.format(pwid.timestamp));
+		scopes.put("utc-sec", secFormat.format(pwid.timestamp));
+		scopes.put("archived-uri", pwid.getUri());
+		scopes.put("pwid", pwid.getUrn());
+
+		Writer writer = new StringWriter();
+		MustacheFactory mf = new DefaultMustacheFactory();
+		Mustache mustache = mf.compile(new StringReader(template), "pathPattern");
+		mustache.execute(writer, scopes);
+		return writer.toString();
+	}
+
 }
